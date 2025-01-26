d1f77b389f8605a313b1db5a46b2bca6d6dd3722
Hajira Mohamed Ali <hajmoham@lab3r2s8.42abudhabi.ae>
2025-01-26 15:46:08 +0400

    minitalk

---
+++ minitalk.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hajmoham <hajmoham@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/25 15:27:37 by hajmoham          #+#    #+#             */
+/*   Updated: 2025/01/25 15:33:57 by hajmoham         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# include <signal.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdint.h>
+# include <stdio.h>
+# include <stddef.h>
+# include "Printf/ft_printf.h"
+
+#endif
